63ae97ad95c94e106868e4a717220064c1026840
muscufacil <102153014+muscufacil@users.noreply.github.com>
2022-03-22 20:32:18 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,83 @@
+<!doctype html>
+<html lang="fr">
+    <head>
+    <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+        <link rel="stylesheet" href="styles.css">
+
+        <title>Accueil | Muscu Facile</title>
+    </head>
+  <body>
+      
+<header>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <img src="muscufacile.png" width="30" height="30" alt="" class="logo">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+
+    <ul class="navbar-nav">
+        <li class="nav-item">
+        <a class="nav-link" href="index.php">Accueil</a>
+        </li>
+        <li class="nav-item">
+        <a class="nav-link" href="#">Musculation</a>
+        </li>
+        <li class="nav-item">
+        <a class="nav-link" href="#">Nutrition</a>
+        </li>
+        <li class="nav-item">
+        <a class="nav-link" href="#">Nos engagements</a>
+        </li>
+        <li class="nav-item">
+        <a class="nav-link" href="#">Contacts</a>
+        </li>
+    </ul>
+    <ul class="navbar-nav ml-md-auto">
+
+    <div >
+                  <a href="tel:05 61 70 24 49"class="btn-5-1 " ><strong>Connectez-vous</strong> </a>
+                  <a href="tel:05 61 70 24 49"class="btn-5" ><strong>Inscrivez vous </strong> </a>
+                </div>
+        
+    </ul>
+    </div>
+    </nav>
+    <div class="principal">
+             <h1 id="acceuil" >Musclez vous facilement grâce à nos exercices ciblés !</h1>
+                
+         </div>
+</header>
+
+<section class="premieres-presentations">
+          <div class="gauche">
+              <h1 id="muscu">Musculation</h1>
+            <p>Nos coachs sportif ont conçus des exercices spécialisés pour chaque muscle que vous souhaitez travailler ! différents programmes sont proposés, pour vous entrainer de chez vous ou bien directement en salle de sport !</p>
+            
+              <a href="muscles.html"class="btn-5" style="margin: bottom 20px;" id="horaires" target="_blank"> <strong>Découvrez les différents exercices !</strong> </a>
+            
+            
+          </div>
+          <div class="droite">
+            <img src="développécouché.jpg" width="400" >
+          </div>
+        </section>
+         
+
+        <!-- Optional JavaScript -->
+        <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  </body>
+</html>
+
+
+
